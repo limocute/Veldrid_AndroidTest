6af08713f7c80754aac1da6823391c17bdc97ff0
limocute <Limocute@limocute.com>
2021-02-25 10:34:34 +0800

    init

---
+++ App1/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using AndroidBase;
+using Veldrid;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private VeldridSurfaceView _view;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            bool debug = false;
+#if DEBUG
+            debug = true;
+#endif
+
+            GraphicsDeviceOptions options = new GraphicsDeviceOptions(
+                debug,
+                PixelFormat.R16_UNorm,
+                false,
+                ResourceBindingModel.Improved,
+                true,
+                true);
+            GraphicsBackend backend = GraphicsDevice.IsBackendSupported(GraphicsBackend.Vulkan)
+                ? GraphicsBackend.Vulkan
+                : GraphicsBackend.OpenGLES;
+            _view = new VeldridSurfaceView(this, backend, options);
+            AndroidApplicationWindow window = new AndroidApplicationWindow(this, _view);
+            window.GraphicsDeviceCreated += (g, r, s) => window.Run();
+            MyApp app = new MyApp(window);
+            SetContentView(_view);
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _view.OnPause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _view.OnResume();
+        }
+
+
+    }
+}
---
+++ App1/MyApp.cs
@@ -0,0 +1,89 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using Veldrid;
+
+namespace App1
+{
+    public class MyApp
+    {
+        private static CommandList _commandList;
+        private Camera _camera;
+
+        private AndroidApplicationWindow Window { get; }
+        private GraphicsDevice GraphicsDevice { get; set; }
+        private ResourceFactory ResourceFactory { get; set; }
+        private Swapchain MainSwapchain { get; set; }
+
+
+        public MyApp(AndroidApplicationWindow window)  
+        {
+            Window = window;
+            Window.Resized += HandleWindowResize;
+            Window.GraphicsDeviceCreated += OnGraphicsDeviceCreated;
+            Window.GraphicsDeviceDestroyed += OnDeviceDestroyed;
+            Window.Rendering += PreDraw;
+            Window.Rendering += Draw;
+
+            _camera = new Camera(Window.Width, Window.Height);
+        }
+
+        private void OnGraphicsDeviceCreated(GraphicsDevice gd, ResourceFactory factory, Swapchain sc)
+        {
+            GraphicsDevice = gd;
+            ResourceFactory = factory;
+            MainSwapchain = sc;
+            CreateResources(factory);
+            CreateSwapchainResources(factory);
+        }
+
+        private void OnDeviceDestroyed()
+        {
+            GraphicsDevice = null;
+            ResourceFactory = null;
+            MainSwapchain = null;
+        }
+        private void CreateSwapchainResources(ResourceFactory factory) { }
+
+        private void CreateResources(ResourceFactory factory)
+        {
+            _commandList = factory.CreateCommandList();
+        }
+
+        private void PreDraw(float deltaSeconds)
+        {
+            _camera.Update(deltaSeconds);
+        }
+
+        private void Draw(float deltaSeconds)
+        {
+            // Begin() must be called before commands can be issued.
+            _commandList.Begin();
+
+            // We want to render directly to the output window.
+            _commandList.SetFramebuffer(GraphicsDevice.SwapchainFramebuffer);
+            _commandList.ClearColorTarget(0, RgbaFloat.Pink);
+
+            _commandList.End();
+            GraphicsDevice.SubmitCommands(_commandList);
+
+            // Once commands have been submitted, the rendered image can be presented to the application window.
+            GraphicsDevice.SwapBuffers();
+        }
+
+        private void HandleWindowResize()
+        {
+            _camera.WindowResized(Window.Width, Window.Height);
+        }
+
+    }
+}
